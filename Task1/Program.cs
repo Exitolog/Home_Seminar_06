390ce9bee16458c19e3284757e36c1c3611f91b7
Sergey <belykov1999@yandex.ru>
2024-02-29 18:34:44 +0300

    Create Task1 in Seminar6

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте двумерный массив символов (тип char [,]).
+// Создать строку из символов этого массива.
+
+void Main()
+{
+
+
+char[,] chars = {{'c', 'a', 'b'},
+{'d','e','b'}};
+string str = "";
+
+foreach(char letter in chars)
+{
+    str+=letter;
+}
+ System.Console.WriteLine(str);
+
+
+
+
+
+
+
+}
+Main();
